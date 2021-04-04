a6f8a964521896d72fdf3abda7e19340497af2d9
Datta <Datta@DESKTOP-706AB6H>
2021-04-04 22:12:26 +0530

    [anant]-sample

---
+++ src/ddd/testNum.java
@@ -0,0 +1,20 @@
+package ddd;
+
+public class testNum {
+	public static void main(String []args)
+	{
+		Integer num1 = 100;
+		Integer num2 = 100;
+		
+		if(num1==num2)
+		{
+			System.out.println("both are equal");
+			
+		}
+		else
+		{
+			System.out.println("both are not equal");
+		}
+	}
+
+}
